bf1e17a85549e585b55f86214786ed9265073b52
allsaidX <allsaidx7@gmail.com>
2021-12-25 19:03:46 +0300

    создан каркас проекта; создана база данных; ипортированы данные в базу

---
+++ signin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace template_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для signin.xaml
+    /// </summary>
+    public partial class signin : Window
+    {
+        public signin()
+        {
+            InitializeComponent();
+        }
+
+        private void tbPwd_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
